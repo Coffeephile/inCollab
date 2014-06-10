54bf6e53ed36bdcb06a34fccf6dd422740e262b4
Coffeephile <coffeephile@live.com>
2014-06-10 22:38:16 +0200

    calendar view

--- public/js/controllers/CalendarCtrl.js
+++ public/js/controllers/CalendarCtrl.js
@@ -1,5 +1,29 @@
-angular.module('CalendarCtrl', []).controller('CalendarController', function($scope) {
+angular.module('CalendarCtrl', ["ui.calendar"]).controller('CalendarController', function($scope) {
 
-	$scope.tagline = 'Wybierz użytkowanika do kontaktu:';	
-
+      $scope.name = "World";
+      $scope.eventSources = [];
+      $scope.next = next;
+      $scope.previous = previous;
+      $scope.error = "";
+      
+      var eventsSource = {};
+      $scope.eventSources.push(eventsSource);
+      
+      function next(){
+        try {
+        $scope.calendar.fullCalendar('next');
+        }
+        catch (err) {
+          $scope.error = err.message;
+        }
+      }
+      
+      function previous(){
+        try {
+        $scope.calendar.fullCalendar('prev');
+        }
+        catch (err) {
+          $scope.error = err.message;
+        }
+      }
 });
